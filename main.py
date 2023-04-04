04de9d8d036843cf41c027befbc04732da67dd00
Vicrrs <enricofermi2019@gmail.com>
2023-04-03 21:30:54 -0400

    primeiro teste

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def index():
+    return "Teste com FASTAPI"
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app="main:app", host="0.0.0.0",
+                port=8000, log_level="info", reload=True)
