001b5bf7adf3915f0540b3638ed80ed6f6e10e9a
Vicrrs <enricofermi2019@gmail.com>
2023-05-30 17:33:30 -0400

    adicionando rota para arquivos estaticos

--- secao02/main.py
+++ secao02/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 @app.get('/')
