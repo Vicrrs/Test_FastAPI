99e3af9190fb050be4119a3b4ba039adbf03cb77
rozas <enricofermi2019@gmail.com>
2023-08-28 01:40:18 -0300

    main

--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
 
 app = FastAPI(docs_url=None, redoc_url=None)
 templates = Jinja2Templates(directory='templates')
@@ -8,7 +9,15 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 app.mount('/media', StaticFiles(directory='media'), name='static')
 
 
+@app.get('/')
+async def index(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('home/index.html', context=context)
+
+
 if __name__ == '__main__':
     import uvicorn
     
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    uvicorn.run(app="main:app", host="localhost", port=8000, log_level="info", reload=True)
